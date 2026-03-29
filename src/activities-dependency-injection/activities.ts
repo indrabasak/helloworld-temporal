312c5e0b533438b7ace4c2447959770903bc8ffc
Indra Basak <indra.basak@autodesk.com>
2026-03-29 16:34:37 -0700

    initial revision of dependency injection example

---
+++ src/activities-dependency-injection/activities.ts
@@ -0,0 +1,14 @@
+export interface DB {
+    get(key: string): Promise<string>;
+}
+
+export const createActivities = (db: DB) => ({
+    async greet(msg: string): Promise<string> {
+        const name = await db.get('name'); // simulate read from db
+        return `${msg}: ${name}`;
+    },
+    async greet_es(mensaje: string): Promise<string> {
+        const name = await db.get('name'); // simulate read from db
+        return `${mensaje}: ${name}`;
+    },
+});
---
+++ src/activities-dependency-injection/worker.ts
@@ -0,0 +1,25 @@
+import {Worker} from '@temporalio/worker';
+
+import {createActivities} from './activities';
+
+async function run() {
+    // Mock DB connection initialization in Worker
+    const db = {
+        async get(_key: string) {
+            return 'Temporal';
+        },
+    };
+
+    const worker = await Worker.create({
+        taskQueue: 'dependency-injection',
+        workflowsPath: require.resolve('./workflows'),
+        activities: createActivities(db),
+    });
+
+    await worker.run();
+}
+
+run().catch((err) => {
+    console.error(err);
+    process.exit(1);
+});
